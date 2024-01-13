982638b67d6b64a6240d99b3bdb606cb67013a13
Bryan Maus <147214872+PeatMaus@users.noreply.github.com>
2024-01-12 17:10:39 -0700

    Removed #change from code line 15

--- Solar Panel Angles.py
+++ Solar Panel Angles.py
@@ -23,3 +23,8 @@ angle_of_sun = math.sin(math.radians((360*julian_spring_day)/365.25))
 solar_declination = max_solar_degrees*angle_of_sun
 angle_of_panel = current_latitude - solar_declination
 print(f"\nAngle of solar panels should be {angle_of_panel} degrees.\n")
+
+#figure length of support in inches
+length_of_solar_panel = 22
+length_of_support = length_of_solar_panel / math.sin(math.radians(angle_of_panel))
+print(f"length of support for 22 inch solar panel: {length_of_support} inches.")
